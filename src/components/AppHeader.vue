a9dc7e86b3ea1b91f15e7b601d31ba99fb2cf871
Serena-Dong <serenadong3159@icloud.com>
2024-04-23 19:23:27 +0200

    app header done

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,7 +15,7 @@ export default {
     <header>
 
         <!-- Logo SONY -->
-        <div class="logo-header bg-black">
+        <div class="logo">
             <div class="container-sm">
                 <div class="logo d-flex justify-content-end align-items-center">
                     <a href=""><img class="logo-header-img" src="./../assets/img/sony_logo.svg" alt=""></a>
@@ -26,11 +26,14 @@ export default {
         <!-- Navbar -->
         <div class="navbar">
             <div class="container-sm">
-                <div class="row w-100 justify-content-between align-items-center">
+                <div class="d-flex w-100 justify-content-between align-items-center">
+
+                    <!-- Navbar Left -->
                     <div class="col-1 col-xxl-8 d-flex">
-                        <a href="">
-                            <img class="logo-navbar-img pt-1" src="./../assets/img/play_logo.svg" alt="">
+                        <!-- Logo -->
+                        <a href=""><img class="logo-navbar-img pt-1" src="./../assets/img/play_logo.svg" alt="">
                         </a>
+                        <!-- Nav-actions -->
                         <div class="nav-actions align-items-center">
                             <ul class="text-black d-flex align-items-center">
                                 <li v-for="link in headerData.navbar">
@@ -40,23 +43,24 @@ export default {
                         </div>
                     </div>
 
+                    <!-- Navbar Right -->
+                    <div class="col-2 col-lg-1 col-xxl-4">
 
-                    <div class="col-1 col-lg-1 col-xxl-4">
+                        <!-- Mobile Menù -->
                         <div class="bars">
-                            <a class="text-secondary" href="">
-                                <i class="fa-bars fa-solid p-2 px-3 border border-grey rounded-3 fs-3"></i>
-                            </a>
+                            <a href=""><i class="fa-bars fa-solid p-2 px-3 border border-grey rounded-3 fs-3"></i></a>
                         </div>
+
+                        <!-- Desktop Menù -->
                         <div class="nav-actions align-items-center justify-content-end">
-                            <ul class="text-black d-flex align-items-center">
-                                <li><a href="">My PlayStation <i
-                                            class="fa-solid fa-chevron-down text-secondary"></i></a></li>
-                                <li><a id="btn-login" href="" class="btn bg-black text-white  mb-0">Accedi</a>
-                                </li>
+                            <ul class="d-flex align-items-center">
+                                <li><a href="">My PlayStation <i class="fa-solid fa-chevron-down text-secondary"></i></a></li>
+                                <li><a id="btn-login" href="" class="btn bg-black text-white  mb-0">Accedi</a></li>
                                 <li><a href=""><i class="fa-solid fa-magnifying-glass"></i></a></li>
                             </ul>
                         </div>
                     </div>
+
                 </div>
             </div>
 
@@ -71,27 +75,37 @@ export default {
 header {
     position: sticky;
     top: 0;
-}
 
-.logo {
-    height: 40px;
-}
+    .logo{
+        background-color: $ps-black;
+        .logo {
+            height: 40px;
+        }
+    }
 
-.navbar,
-.nav-actions {
-    background-color: $ps-white;
-    height: 80px;
-}
+    .navbar{
+        background-color: $ps-white;
 
-.nav-actions ul>* {
-    margin-left: 15px;
-}
+        &, .nav-actions{
+            height: 80px;
+        }
+        
+        .nav-actions ul>* {
+            margin-left: 15px;
+        }
+        
+        .nav-actions ul li>* {
+            color: $ps-black;
+        }
 
-.nav-actions ul li>* {
-    color: #000000;
-}
+        .bars a {
+            color: $ps-grey;
+        }
+    }
 
-.nav-actions {
-    display: none;
+
+    .nav-actions {
+        display: none;
+    }
 }
 </style>
