95db45015e9a6aaebcc9a7f3d77287dc7c2ec04a
Serena-Dong <serenadong3159@icloud.com>
2024-04-27 20:06:54 +0200

    banner setup

---
+++ src/components/contents/Banner.vue
@@ -0,0 +1,55 @@
+<script>
+import Button from './Button.vue';
+
+export default {
+    name: 'Banner',
+    components: { Button },
+    props: {
+        order: String,
+        logo: String,
+        description: String,
+        button: Array,
+        img: String,
+        bgColor: String,
+        color: String
+    }
+}
+</script>
+
+<template>
+    <!-- Color of the banner -->
+    <section class="banner" :class="[bgColor, order]">
+        <div class="container-sm">
+            <div class="row align-items-center">
+
+                <!-- Banner Text -->
+                <div class="banner-text col-12 col-lg-6">
+                    <div class="text py-5">
+                        <img :src="logo" alt="">
+                        <p :class="color">{{ description }}</p>
+                        <Button :button="button"></Button>
+                    </div>
+                </div>
+
+                <!-- Banner Img -->
+                <div class="banner-img col-lg-6 img-none">
+                    <img class="img-fluid" :src="img" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.banner {
+    .banner-text {
+        img {
+            width: 50%;
+        }
+
+        p {
+            padding: 3rem 0;
+        }
+    }
+}
+</style>
